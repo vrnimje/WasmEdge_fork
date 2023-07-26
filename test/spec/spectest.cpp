66f763556df75962a7f94131a496a5a924a4c629
Vedant R. Nimje <vedantnimjed@gmail.com>
2023-07-26 13:38:22 +0530

    Binary compare literal used
    Signed-off-by: Vedant R. Nimje <vedantnimjed@gmail.com>

--- test/spec/spectest.cpp
+++ test/spec/spectest.cpp
@@ -642,7 +642,7 @@ void SpecTest::run(std::string_view Proposal, std::string_view UnitName) {
       case CommandID::AssertMalformed: {
         std::string_view ModType = Cmd["module_type"];
         std::string ModStr = (std::string) ModType;
-        if (ModStr == "binary") {
+        if (ModStr.compare("binary")) {
           // TODO: Wat is not supported in WasmEdge yet.
           return;
         }
